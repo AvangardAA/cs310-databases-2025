a59b4563bc3af9988639169c9abbcf1e2c048c8e
AvangardAA <artemnzr@gmail.com>
2025-10-23 21:46:31 +0000

    Add creation queries

---
+++ homework2/DDL/create_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE customers(
+	customers_id INTEGER PRIMARY KEY,
+	customer_name TEXT,
+	region TEXT
+);
+
+CREATE TABLE orders(
+	order_id INTEGER PRIMARY KEY,
+	customer_id INTEGER,
+	product_id INTEGER,
+	order_date DATE,
+	quantity INTEGER,
+	total_amt DOUBLE
+);
+
+CREATE TABLE products(
+	product_id INTEGER PRIMARY KEY,
+	product_name TEXT,
+	category TEXT,
+	price DOUBLE
+);
